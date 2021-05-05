52206c18f296c38b26a36d3be7b9e7f4dacfc6d8
Brandon Chavez <brandedhard@gmail.com>
2021-05-05 12:26:04 -0700

    Fixed background to grow or shrink

--- client/UI.jsx
+++ client/UI.jsx
@@ -9,7 +9,7 @@ class UI extends React.Component{
           <div className={'container card'}>
             <img src={'./images/sun.svg'} alt='sun' width='200' height='200'></img>
             <div className={'temp'}>Tempurature 72&deg;</div>
-            <div className={'location'}>Location</div>
+            <div className={'location'}>Location: Los Angeles</div>
             <div className={'buttonContainer'}>
               <button>Favorite Locations</button>
             </div>
