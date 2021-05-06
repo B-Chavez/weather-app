d0692f8372f27bd55d1bace78daf8ec03d224397
Brandon Chavez <brandedhard@gmail.com>
2021-05-05 18:58:13 -0700

    css styling for mobile width

--- client/UI.jsx
+++ client/UI.jsx
@@ -5,6 +5,9 @@ class UI extends React.Component{
   render(){
     return(
       <div>
+        <div className={'navbar'}>
+          <img src="./images/home.svg" alt="home button"/>
+        </div>
         <div className={'outerContainer'}>
           <div className={'container card'}>
             <img src={'./images/sun.svg'} alt='sun' width='200' height='200'></img>
