ce696ed4fdfcb00b6ca0883a78730cc140904bbf
Brandon Chavez <brandedhard@gmail.com>
2021-05-07 18:22:44 -0700

    fixed class names

--- client/UI.jsx
+++ client/UI.jsx
@@ -5,15 +5,15 @@ class UI extends React.Component{
   render(){
     return(
       <div>
-        <div className={'navbar'}>
+        <div className='navbar'>
           <img src="./images/home.svg" alt="home button"/>
         </div>
-        <div className={'outerContainer'}>
-          <div className={'container card'}>
-            <img src={'./images/sun.svg'} alt='sun' width='200' height='200'></img>
-            <div className={'temp'}>Tempurature 72&deg;</div>
-            <div className={'location'}>Location: Los Angeles</div>
-            <div className={'buttonContainer'}>
+        <div className='outerContainer'>
+          <div className='container card'>
+            <img src='./images/sun.svg' alt='sun' width='200' height='200'></img>
+            <div className='temp'>Tempurature 72&deg;</div>
+            <div className='location'>Location: Los Angeles</div>
+            <div className='buttonContainer'>
               <button>Favorite Locations</button>
             </div>
           </div>
